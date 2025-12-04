380c0b4cd9a5971d15d4be0a65c06428c71511da
{{your_name}} <tauhidrana00@gmail.com>
2025-12-04 10:58:14 +0600

    CPP  problem solved

---
+++ Chef_Fantasy_11.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        int ans = n * (n - 1);
+        cout << ans << endl;
+    }
+
+    return 0;
+}
